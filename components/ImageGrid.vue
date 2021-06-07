ab48666d8228ce1f51ec488d6f8d513da4cbc8bf
Kelvin Ngunyi <ngunyimacharia@gmail.com>
2021-06-07 20:23:25 +0200

    Lazy load images

--- components/ImageGrid.vue
+++ components/ImageGrid.vue
@@ -1,8 +1,6 @@
 <template>
   <div>
     <div class="p-5 mx-auto max-w-screen-xl">
-      <h2 class="font-bold uppercase text-xl pb-4">Best sellers</h2>
-
       <div
         class="
           grid grid-flow-row-dense grid-cols-2
@@ -12,145 +10,32 @@
           md:grid-cols-4
         "
       >
-        <div>
-          <img
-            class="
-              border border-yellow-700
-              mb-1
-              border-solid
-              w-full
-              hover:border-yellow-500
-            "
-            alt="Best seller"
-            src="https://images.unsplash.com/photo-1594760467096-03761ecedde4?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzE2fHxsaWdodGluZ3xlbnwwfDJ8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+        <div v-for="image in board.images" :key="image.public_id">
+          <cld-image
+            :public-id="image.public_id"
+            width="200"
+            height="200"
+            crop="fill"
+            gravity="auto:subject"
+            fetchFormat="auto"
+            quality="auto"
+            class="mb-1 border-solid w-full hover:border-yellow-500"
+            :alt="`${image.public_id} image`"
             loading="lazy"
           />
-          <h2 class="pt-2 m-0 leading-4 font-semibold">
-            Indoor light 2021 USA
-          </h2>
-          <p>$210</p>
-          <p class="text-green-500 italic font-medium">In Stock</p>
-        </div>
-        <div class="">
-          <img
-            class="
-              border border-yellow-700 border-solid
-              w-full
-              hover:border-yellow-500
-            "
-            alt="Best seller"
-            src="https://images.unsplash.com/photo-1592009104079-88046bd5a09a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzQwfHxsaWdodGluZ3xlbnwwfDJ8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
-            loading="lazy"
-          />
-          <h2 class="pt-2 m-0 leading-4 font-semibold">
-            Indoor light 2021 USA
-          </h2>
-          <p>$210</p>
-          <p class="text-green-500 italic font-medium">In Stock</p>
-        </div>
-        <div class="">
-          <img
-            class="
-              border border-yellow-700 border-solid
-              w-full
-              hover:border-yellow-500
-            "
-            alt="Best seller"
-            src="https://images.unsplash.com/photo-1562635254-c14536b57bcb?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDd8fGxpZ2h0aW5nfGVufDB8MnwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
-            loading="lazy"
-          />
-          <h2 class="pt-2 m-0 leading-4 font-semibold">
-            Indoor light 2021 USA
-          </h2>
-          <p>$210</p>
-          <p class="text-green-500 italic font-medium">In Stock</p>
-        </div>
-        <div class="">
-          <img
-            class="
-              border border-yellow-700 border-solid
-              w-full
-              hover:border-yellow-500
-            "
-            alt="Best seller"
-            src="https://images.unsplash.com/photo-1595389397394-01fed35d24bc?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTAwfHxsaWdodGluZ3xlbnwwfDJ8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
-            loading="lazy"
-          />
-          <h2 class="pt-2 m-0 leading-4 font-semibold">
-            Outdoor light 2021 USA
-          </h2>
-          <p>$210</p>
-          <p class="text-red-500 italic font-medium">Out of Stock</p>
-        </div>
-        <div class="">
-          <img
-            class="
-              border border-yellow-700 border-solid
-              w-full
-              hover:border-yellow-500
-            "
-            alt="Best seller"
-            src="https://images.unsplash.com/photo-1577430605074-47eb89cc8969?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTExfHxsaWdodGluZ3xlbnwwfDJ8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
-            loading="lazy"
-          />
-          <h2 class="pt-2 m-0 leading-4 font-semibold">
-            Indoor light 2020 USA
-          </h2>
-          <p>$210</p>
-          <p class="text-green-500 italic font-medium">In Stock</p>
-        </div>
-        <div class="">
-          <img
-            class="
-              border border-yellow-700 border-solid
-              w-full
-              hover:border-yellow-500
-            "
-            alt="Best seller"
-            src="https://images.unsplash.com/photo-1565475266382-7870244b60f9?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjYzfHxsaWdodGluZ3xlbnwwfDJ8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
-            loading="lazy"
-          />
-          <h2 class="pt-2 m-0 leading-4 font-semibold">
-            Indoor light 2021 USA
-          </h2>
-          <p>$210</p>
-          <p class="text-green-500 italic font-medium">In Stock</p>
-        </div>
-        <div class="">
-          <img
-            class="
-              border border-yellow-700 border-solid
-              w-full
-              hover:border-yellow-500
-            "
-            alt="Best seller"
-            src="https://images.unsplash.com/photo-1588707698376-559b54b41846?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjgyfHxsaWdodGluZ3xlbnwwfDJ8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
-            loading="lazy"
-          />
-          <h2 class="pt-2 m-0 leading-4 font-semibold">
-            Outdoor light 2020 USA
-          </h2>
-          <p>$210</p>
-          <p class="text-red-500 italic font-medium">Out of Stock</p>
-        </div>
-        <div class="">
-          <img
-            class="
-              border border-yellow-700 border-solid
-              w-full
-              hover:border-yellow-500
-            "
-            alt="Best seller"
-            src="https://images.unsplash.com/photo-1533637268824-0da337d92744?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzAyfHxsaWdodGluZ3xlbnwwfDJ8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
-            loading="lazy"
-          />
-          <h2 class="pt-2 m-0 leading-4 font-semibold">
-            Outdoor light 2021 USA
-          </h2>
-          <p>$210</p>
-          <p class="text-green-500 italic font-medium">In Stock</p>
         </div>
       </div>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  props: {
+    board: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
