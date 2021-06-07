4339efa557e07baecef6bc9c161e174fbeafba18
Kelvin Ngunyi <ngunyimacharia@gmail.com>
2021-06-07 20:52:37 +0200

    Background image lazy loading added

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,37 +1,24 @@
 <template>
-  <div class="bg-black">
-    <div class="flex relative text-center">
-      <h1
-        class="
-          text-3xl
-          tracking-wider
-          text-white text-sha
-          uppercase
-          font-bold
-          p-4
-          self-center
-          z-10
-          content-center
-          absolute
-          text-center
-          w-full
-          md:text-4xl
-        "
-      >
-        {{ board.name }}
-      </h1>
-      <cld-image
-        class="w-full object-cover h-72 block mx-auto sm:block sm:w-full"
-        width="1920"
-        height="288"
-        :public-id="backgroundImage.public_id"
-        gravity="auto:subject"
-        fetchFormat="auto"
-        quality="auto"
-        :alt="`${board.name} hero image`"
-        loading="lazy"
-      />
-    </div>
+  <div class="flex w-full h-72 text-center" :lazy-background="backgroundUrl">
+    <h1
+      class="
+        text-3xl
+        tracking-wider
+        text-white text-sha
+        uppercase
+        font-bold
+        p-4
+        self-center
+        z-10
+        content-center
+        absolute
+        text-center
+        w-full
+        md:text-4xl
+      "
+    >
+      {{ board.name }}
+    </h1>
   </div>
 </template>
 
@@ -44,10 +31,19 @@ export default {
     },
   },
   computed: {
-    backgroundImage() {
+    backgroundUrl() {
       const image =
         this.board.images[Math.floor(Math.random() * this.board.images.length)]
-      return image
+
+      return this.$cloudinary.image.url(image.public_id, {
+        class: 'w-full object-cover h-72 block mx-auto sm:block sm:w-full',
+        width: '1920',
+        height: '288',
+        gravity: 'auto:subject',
+        fetchFormat: 'auto',
+        quality: 'auto',
+        crop: 'fill',
+      })
     },
   },
 }
